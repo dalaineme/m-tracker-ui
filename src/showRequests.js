b7cf72e3fbf581b9fcdb5e1c77a175121f62481d
dcDalin <mcdalinoluoch@gmail.com>
2018-06-27 18:21:17 +0300

    :book: [Chore #158588789] Indent looped HTML result

--- src/showRequests.js
+++ src/showRequests.js
@@ -17,7 +17,6 @@ async function fetchRequests() {
     // only proceed once promise is resolved
     let data = await response.json();
     // only proceed once second promise is resolved
-
     return data;
   } catch (err) {
     console.error(err);
