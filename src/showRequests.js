51330924e6249fa8298f0175c91281af9fd97ccf
dcDalin <mcdalinoluoch@gmail.com>
2018-06-27 17:24:26 +0300

    :book: [Chore #158588789] Indent looped HTML result

--- src/showRequests.js
+++ src/showRequests.js
@@ -40,36 +40,34 @@ setTimeout(() => {
         } = user;
 
         result += `
-    <div class="request-top" id="request-top">
-      <div class="request-top-left" id="request-top-left">
-        <i class="far fa-calendar request-icons"></i>
-        Request ID: ${request_id}
-      </div>
-      <div class="request-top-right" id="request-top-right">
-        <i class="fas fa-user request-icons"></i>
-        ${email}
-      </div>
-    </div>
-    <div class="request-mid">
-      <div class="request-mid-left" id="request-title">
-        <i class="fas fa-toolbox request-icons"></i>
-        ${request_title.substr(0, 60)}...
-      </div>
-      <div class="request-mid-right">
-        <span class="status-pending" id="the-status">${current_status}</span>
-      </div>
-    </div>
-    <div class="request-body" id="request-body">
-      <p class="request-body-p">
-        ${request_description.substr(0, 120)}...
-        <button class="submit" ">More Details
-          <i class="fas fa-chevron-circle-down"></i>
-        </button>
-      </p>
-    </div>
-    
-    `;
-
+          <div class="request-top" id="request-top">
+            <div class="request-top-left" id="request-top-left">
+              <i class="far fa-calendar request-icons"></i>
+              Request ID: ${request_id}
+            </div>
+            <div class="request-top-right" id="request-top-right">
+              <i class="fas fa-user request-icons"></i>
+              ${email}
+            </div>
+          </div>
+          <div class="request-mid">
+            <div class="request-mid-left" id="request-title">
+              <i class="fas fa-toolbox request-icons"></i>
+              ${request_title.substr(0, 60)}...
+            </div>
+            <div class="request-mid-right">
+              <span class="status-pending" id="the-status">${current_status}</span>
+            </div>
+          </div>
+          <div class="request-body" id="request-body">
+            <p class="request-body-p">
+              ${request_description.substr(0, 120)}...
+              <button class="submit" ">More Details
+                <i class="fas fa-chevron-circle-down"></i>
+              </button>
+            </p>
+          </div> 
+        `;
         getElement("request-wrapper", result);
       });
     })
